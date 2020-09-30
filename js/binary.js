af92a7f12cb5185e9bc2c1b7c1b0686d01b6e75c
System Administrator <root@Akmals-MacBook-Pro.local>
2020-09-30 10:03:08 +0800

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -14090,7 +14090,7 @@ var Validation = function () {
         } else if ('min' in options && 'max' in options && +options.min === +options.max && +value !== +options.min) {
             is_ok = false;
             message = localize('Should be [_1]', addComma(options.min, options.format_money ? getDecimalPlaces(Client.get('currency')) : undefined));
-        } else if ('min' in options && 'max' in options && (+value < +options.min || isMoreThanMax(value, options))) {
+        } else if ('min' in options && 'max' in options && (+value <= +options.min || isMoreThanMax(value, options))) {
             is_ok = false;
             message = localize('Should be between [_1] and [_2]', [addComma(options.min, options.format_money ? getDecimalPlaces(Client.get('currency')) : undefined), addComma(options.max, options.format_money ? getDecimalPlaces(Client.get('currency')) : undefined)]);
         } else if ('min' in options && +value < +options.min) {
@@ -30998,7 +30998,6 @@ var SelfExclusion = function () {
                     }
                     $form.find('#' + key).attr('disabled', has_exclude_until).val(value);
                 });
-                $form.find('#btn_submit').setVisibility(!has_exclude_until);
 
                 $('#chk_no_limit').on('change', function () {
                     setMax30DayTurnoverLimit($(this).is(':checked'));
