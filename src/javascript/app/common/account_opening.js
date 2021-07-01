b58a405e40c9070f917d0826ab0273f3b9f6b73d
Akmal Djumakhodjaev <akmal@binary.com>
2021-07-01 12:02:48 +0800

    Make address state required field for australian clients

--- src/javascript/app/common/account_opening.js
+++ src/javascript/app/common/account_opening.js
@@ -284,7 +284,7 @@ const AccountOpening = (() => {
             { selector: '#address_line_1',              validations: ['req', 'address', ['length', { min: 1, max: 70 }]] },
             { selector: '#address_line_2',              validations: ['address', ['length', { min: 0, max: 70 }]] },
             { selector: '#address_city',                validations: ['req', 'letter_symbol', ['length', { min: 1, max: 35 }]] },
-            { selector: '#address_state',               validations: $('#address_state').prop('nodeName') === 'SELECT' ? '' : ['letter_symbol', ['length', { min: 0, max: 35 }]] },
+            { selector: '#address_state',               validations: $('#address_state').prop('nodeName') === 'SELECT' ? Client.get('residence') === 'au' ? ['req'] : '' : ['letter_symbol', ['length', { min: 0, max: 35 }]] },
             { selector: '#address_postcode',            validations: [Client.get('residence') === 'gb' || State.getResponse('authorize.upgradeable_landing_companies').some(lc => lc === 'iom') ? 'req' : '', 'postcode', ['length', { min: 0, max: 20 }]] },
             { selector: '#phone',                       validations: ['req', 'phone', ['length', { min: 9, max: 35, value: () => $('#phone').val().replace(/\D/g,'') }]] },
             { selector: '#secret_question',             validations: ['req'] },
